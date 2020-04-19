af6cb1ddcd30098c9dbda14665d9a17e67887c4a
Christopher Kirkley <ckirkley@gmail.com>
2020-04-19 14:03:22 -0700

    Initial commit.

---
+++ functional_test.py
@@ -0,0 +1,20 @@
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+
+class BrowserTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path = '/Users/ck/python/geckodriver/geckodriver')
+        self.browser.get('http://localhost:8000')
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_artist_page_loads(self):
+        title = self.browser.title
+        self.assertEqual(title, 'Artists')
+
+if __name__ == "__main__":
+    unittest.main()
+
